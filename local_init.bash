8f7c63bd9c45ed0390ea0c328f30bf2979cd830a
assafmo <assaf.morami@gmail.com>
2019-12-18 15:00:21 +0200

    local_init.bash also get enigma-js.node.js and create .env

--- local_init.bash
+++ local_init.bash
@@ -5,3 +5,8 @@ mkdir -p ../build
 docker cp $CONTAINER_ID:/root/build/contracts ../build
 sed -e 's_http://contract_http://localhost_g;s_http://bootstrap_http://localhost_g' ../build/contracts/addresses.json > ../build/contracts/addresses.new
 mv ../build/contracts/addresses.new ../build/contracts/addresses.json
+
+echo "SGX_MODE=SW" > .env
+echo "ENIGMA_ENV=COMPOSE" >> .env
+
+wget -P enigma-js/lib https://raw.githubusercontent.com/enigmampc/enigma-contract/develop/enigma-js/lib/enigma-js.node.js
