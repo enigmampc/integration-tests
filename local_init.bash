0ab93a3596eee3b2ceb72e543e84f687443df4f9
assafmo <assaf.morami@gmail.com>
2019-12-26 13:16:56 +0200

    local_init.bash: set -xe, clean ../build before writing to it

--- local_init.bash
+++ local_init.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+set -xe
+
 CONTAINER_ID=$(docker container ls | grep enigmampc/client | cut -d' ' -f1)
+
 mkdir -p ~/.enigma
+
+rm -rf ../build
 mkdir -p ../build
+
 docker cp $CONTAINER_ID:/root/build/contracts ../build
 sed -e 's_http://contract_http://localhost_g;s_http://bootstrap_http://localhost_g' ../build/contracts/addresses.json > ../build/contracts/addresses.new
 mv ../build/contracts/addresses.new ../build/contracts/addresses.json
