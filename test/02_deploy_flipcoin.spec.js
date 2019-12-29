5f8e3f420365d8f792bf18077a2adab940dd42f5
assafmo <assaf.morami@gmail.com>
2019-12-29 15:41:10 +0200

    deploy_flipcoin is now again just like deploy_calculator
    They were almost identical before the recent changes to deploy_calculator, so I applied the same refactor to deploy_flipcoin

--- test/02_deploy_flipcoin.spec.js
+++ test/02_deploy_flipcoin.spec.js
@@ -1,92 +1,76 @@
 /* eslint-disable require-jsdoc */
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
-import Web3 from 'web3';
-import {Enigma, utils, eeConstants} from './enigmaLoader';
-import {EnigmaContract, EnigmaTokenContract, EnigmaContractAddress, EnigmaTokenContractAddress,
-  proxyAddress, ethNodeAddr} from './contractLoader';
-import * as constants from './testConstants';
-
+import fs from "fs";
+import os from "os";
+import path from "path";
+import Web3 from "web3";
+import { Enigma, utils, eeConstants } from "./enigmaLoader";
+import { EnigmaContractAddress, EnigmaTokenContractAddress, proxyAddress, ethNodeAddr } from "./contractLoader";
+import * as constants from "./testConstants";
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-describe('Enigma tests', () => {
+describe("Enigma tests", () => {
   let accounts;
   let web3;
   let enigma;
-  let epochSize;
-  it('initializes', () => {
-    const provider = new Web3.providers.HttpProvider(ethNodeAddr);
-    web3 = new Web3(provider);
-    return web3.eth.getAccounts().then((result) => {
-      accounts = result;
-      enigma = new Enigma(
-        web3,
-        EnigmaContractAddress,
-        EnigmaTokenContractAddress,
-        proxyAddress,
-        {
-          gas: 4712388,
-          gasPrice: 100000000000,
-          from: accounts[0],
-        },
-      );
-      enigma.admin();
-      enigma.setTaskKeyPair('cupcake');
-      expect(Enigma.version()).toEqual('0.0.1');
+
+  beforeAll(async () => {
+    web3 = new Web3(new Web3.providers.HttpProvider(ethNodeAddr));
+    accounts = await web3.eth.getAccounts();
+    enigma = new Enigma(web3, EnigmaContractAddress, EnigmaTokenContractAddress, proxyAddress, {
+      gas: 4712388,
+      gasPrice: 100000000000,
+      from: accounts[0]
     });
+    enigma.admin();
+    enigma.setTaskKeyPair("cupcake");
+    expect(Enigma.version()).toEqual("0.0.1");
   });
 
-  let scTask;
-  let task;
-  const homedir = os.homedir();
+  it(
+    "should deploy secret contract",
+    async () => {
+      const scTaskFn = "construct()";
+      const scTaskArgs = "";
+      const scTaskGasLimit = 1000000;
+      const scTaskGasPx = utils.toGrains(1);
+      const preCode = fs.readFileSync(path.resolve(__dirname, "secretContracts/flipcoin.wasm"));
 
-  it('should deploy secret contract', async () => {
-    let scTaskFn = 'construct()';
-    let scTaskArgs = '';
-    let scTaskGasLimit = 1000000;
-    let scTaskGasPx = utils.toGrains(1);
-    let preCode;
-    try {
-      preCode = fs.readFileSync(path.resolve(__dirname,'secretContracts/flipcoin.wasm'));
-    } catch(e) {
-      console.log('Error:', e.stack);
-    }
-    scTask = await new Promise((resolve, reject) => {
-      enigma.deploySecretContract(scTaskFn, scTaskArgs, scTaskGasLimit, scTaskGasPx, accounts[0], preCode,
-        constants.RETRIES_DEPLOY)
-        .on(eeConstants.DEPLOY_SECRET_CONTRACT_RESULT, (receipt) => resolve(receipt))
-        .on(eeConstants.ERROR, (error) => reject(error));
-    });
+      const scTask = await new Promise((resolve, reject) => {
+        enigma
+          .deploySecretContract(
+            scTaskFn,
+            scTaskArgs,
+            scTaskGasLimit,
+            scTaskGasPx,
+            accounts[0],
+            preCode,
+            constants.RETRIES_DEPLOY
+          )
+          .on(eeConstants.DEPLOY_SECRET_CONTRACT_RESULT, resolve)
+          .on(eeConstants.ERROR, reject);
+      });
 
-    fs.writeFile(path.join(homedir, '.enigma', 'addr-flipcoin.txt'), scTask.scAddr, 'utf8', function(err) {
-      if(err) {
-        return console.log(err);
+      fs.writeFileSync("/tmp/enigma/addr-flipcoin.txt", scTask.scAddr, "utf8");
+
+      let scTaskStatus;
+      while (true) {
+        scTaskStatus = await enigma.getTaskRecordStatus(scTask);
+        if (scTaskStatus.ethStatus == 2) {
+          break;
+        }
+        await sleep(1000);
       }
-    });
-  }, constants.TIMEOUT_DEPLOY);
+      expect(scTaskStatus.ethStatus).toEqual(2);
 
-  it('should get the confirmed deploy contract task', async () => {
-    do {
-      await sleep(1000);
-      scTask = await enigma.getTaskRecordStatus(scTask);
-      process.stdout.write('Waiting. Current Task Status is '+scTask.ethStatus+'\r');
-    } while (scTask.ethStatus != 2);
-    expect(scTask.ethStatus).toEqual(2);
-    process.stdout.write('Completed. Final Task Status is '+scTask.ethStatus+'\n');
-  }, constants.TIMEOUT_DEPLOY);
+      const isDeployed = await enigma.admin.isDeployed(scTask.scAddr);
+      expect(isDeployed).toEqual(true);
 
-  it('should verify deployed contract', async () => {
-    const result = await enigma.admin.isDeployed(scTask.scAddr);
-    expect(result).toEqual(true);
-  });
-
-  it('should get deployed contract bytecode hash', async () => {
-    const result = await enigma.admin.getCodeHash(scTask.scAddr);
-    expect(result).toBeTruthy;
-    console.log('Deployed contract bytecode hash is: '+result);
-  });
+      const codeHash = await enigma.admin.getCodeHash(scTask.scAddr);
+      expect(codeHash).toBeTruthy();
+    },
+    constants.TIMEOUT_DEPLOY
+  );
 });
