880cf7e47f0bd8ad39693cb9dc093c775be9bb39
assafmo <assaf.morami@gmail.com>
2019-12-30 10:16:35 +0200

    refactor deploy_voting

--- test/02_deploy_voting.spec.js
+++ test/02_deploy_voting.spec.js
@@ -1,20 +1,19 @@
 /* eslint-disable require-jsdoc */
 const fs = require("fs");
-import os from "os";
 const path = require("path");
-const Web3 = require("Web3");
-import { Enigma, utils, eeConstants } from "./enigmaLoader";
-import {
-  EnigmaContract,
-  EnigmaTokenContract,
-  VotingETHContract,
+const Web3 = require("web3");
+const { Enigma } = require("./enigmaLoader");
+const {
   EnigmaContractAddress,
   EnigmaTokenContractAddress,
   proxyAddress,
   ethNodeAddr,
+  VotingETHContract,
   VotingETHContractAddress
-} from "./contractLoader";
-import * as constants from "./testConstants";
+} = require("./contractLoader");
+const constants = require("./testConstants");
+
+const { deploy } = require("./deploy");
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -24,91 +23,53 @@ describe("Enigma tests", () => {
   let accounts;
   let web3;
   let enigma;
-  let votingETHContract;
-  let epochSize;
-  it("initializes", () => {
-    const provider = new Web3.providers.HttpProvider(ethNodeAddr);
-    web3 = new Web3(provider);
-    return web3.eth.getAccounts().then(result => {
-      accounts = result;
-      enigma = new Enigma(web3, EnigmaContractAddress, EnigmaTokenContractAddress, proxyAddress, {
-        gas: 4712388,
-        gasPrice: 100000000000,
-        from: accounts[0]
-      });
-      enigma.admin();
-      enigma.setTaskKeyPair("cupcake");
-      expect(Enigma.version()).toEqual("0.0.1");
+
+  beforeAll(async () => {
+    web3 = new Web3(new Web3.providers.HttpProvider(ethNodeAddr));
+    accounts = await web3.eth.getAccounts();
+    enigma = new Enigma(web3, EnigmaContractAddress, EnigmaTokenContractAddress, proxyAddress, {
+      gas: 4712388,
+      gasPrice: 100000000000,
+      from: accounts[0]
     });
+    enigma.admin();
+    enigma.setTaskKeyPair("cupcake");
+    expect(Enigma.version()).toEqual("0.0.1");
   });
 
-  it("initializes VotingETH contract", async () => {
-    votingETHContract = new enigma.web3.eth.Contract(VotingETHContract["abi"], VotingETHContractAddress);
-    expect(votingETHContract.options.address).toBeTruthy();
-  });
-
-  let scTask;
-  let task;
-  const homedir = os.homedir();
   it(
     "should deploy secret contract",
     async () => {
-      let scTaskFn = `construct(address)`;
-      let scTaskArgs = [[votingETHContract.options.address, "address"]];
-      let scTaskGasLimit = 4000000;
-      let scTaskGasPx = utils.toGrains(1);
-      let preCode;
-      try {
-        preCode = fs.readFileSync(path.resolve(__dirname, "secretContracts/voting.wasm"));
-      } catch (e) {
-        console.log("Error:", e.stack);
-      }
-      scTask = await new Promise((resolve, reject) => {
-        enigma
-          .deploySecretContract(
-            scTaskFn,
-            scTaskArgs,
-            scTaskGasLimit,
-            scTaskGasPx,
-            accounts[0],
-            preCode,
-            constants.RETRIES_DEPLOY
-          )
-          .on(eeConstants.DEPLOY_SECRET_CONTRACT_RESULT, receipt => resolve(receipt))
-          .on(eeConstants.ERROR, error => reject(error));
-      });
+      const { options } = new enigma.web3.eth.Contract(VotingETHContract["abi"], VotingETHContractAddress);
+      expect(options.address).toEqual(VotingETHContractAddress);
 
-      fs.writeFile(path.join(homedir, ".enigma", "addr-voting.txt"), scTask.scAddr, "utf8", function(err) {
-        if (err) {
-          return console.log(err);
+      const scTask = await deploy(
+        enigma,
+        accounts[0],
+        path.resolve(__dirname, "secretContracts/voting.wasm"),
+        "construct(address)",
+        [[VotingETHContractAddress, "address"]],
+        4000000
+      );
+
+      fs.writeFileSync("/tmp/enigma/addr-voting.txt", scTask.scAddr, "utf8");
+
+      let scTaskStatus;
+      while (true) {
+        scTaskStatus = await enigma.getTaskRecordStatus(scTask);
+        if (scTaskStatus.ethStatus == 2) {
+          break;
         }
-      });
-    },
-    constants.TIMEOUT_DEPLOY
-  );
-
-  it(
-    "should get the confirmed deploy contract task",
-    async () => {
-      do {
         await sleep(1000);
-        scTask = await enigma.getTaskRecordStatus(scTask);
-        process.stdout.write("Waiting. Current Task Status is " + scTask.ethStatus + "\r");
-      } while (scTask.ethStatus != 2);
-      expect(scTask.ethStatus).toEqual(2);
-      process.stdout.write("Completed. Final Task Status is " + scTask.ethStatus + "\n");
+      }
+      expect(scTaskStatus.ethStatus).toEqual(2);
+
+      const isDeployed = await enigma.admin.isDeployed(scTask.scAddr);
+      expect(isDeployed).toEqual(true);
+
+      const codeHash = await enigma.admin.getCodeHash(scTask.scAddr);
+      expect(codeHash).toBeTruthy();
     },
     constants.TIMEOUT_DEPLOY
   );
-
-  it("should verify deployed contract", async () => {
-    const result = await enigma.admin.isDeployed(scTask.scAddr);
-    expect(result).toEqual(true);
-  });
-
-  it("should get deployed contract bytecode hash", async () => {
-    const result = await enigma.admin.getCodeHash(scTask.scAddr);
-    expect(result).toBeTruthy;
-    console.log("Deployed contract bytecode hash is: " + result);
-  });
 });
