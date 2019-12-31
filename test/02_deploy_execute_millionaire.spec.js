46890fc6ddb6370a4fcdf9dd02536e01e63580ca
assafmo <assaf.morami@gmail.com>
2019-12-31 10:40:11 +0200

    test/02_deploy_execute_millionaire.spec.js

--- test/02_deploy_execute_millionaire.spec.js
+++ test/02_deploy_execute_millionaire.spec.js
@@ -1,21 +1,17 @@
-/* eslint-disable require-jsdoc */
 const fs = require("fs");
 const path = require("path");
 const Web3 = require("web3");
-const { Enigma, eeConstants } = require("./enigmaLoader");
+const { Enigma, eeConstants, utils } = require("./enigmaLoader");
 const { EnigmaContractAddress, EnigmaTokenContractAddress, proxyAddress, ethNodeAddr } = require("./contractLoader");
 const constants = require("./testConstants");
 
-const { deploy } = require("./scUtils");
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const { deploy, testComputeHelper, sleep } = require("./scUtils");
 
 describe("millionaire", () => {
   let accounts;
   let web3;
   let enigma;
+  let scAddr;
 
   beforeAll(async () => {
     web3 = new Web3(new Web3.providers.HttpProvider(ethNodeAddr));
@@ -31,10 +27,10 @@ describe("millionaire", () => {
   });
 
   it(
-    "should deploy secret contract",
+    "deploy",
     async () => {
       const deployTask = await deploy(enigma, accounts[0], path.resolve(__dirname, "secretContracts/millionaire.wasm"));
-
+      scAddr = deployTask.scAddr;
       fs.writeFileSync("/tmp/enigma/addr-millionaire.txt", deployTask.scAddr, "utf8");
 
       while (true) {
@@ -54,5 +50,47 @@ describe("millionaire", () => {
       expect(codeHash).toBeTruthy();
     },
     constants.TIMEOUT_DEPLOY
+  );
+
+  const millionaire1 = "0x0000000000000000000000000000000000000000000000000000000000000001";
+  const millionaire2 = "0x0000000000000000000000000000000000000000000000000000000000000002";
+
+  it(
+    "computeTask add_millionaire",
+    async () => {
+      await testComputeHelper(
+        enigma,
+        accounts[0],
+        scAddr,
+        "add_millionaire(bytes32,uint256)",
+        [
+          [millionaire1, "bytes32"],
+          [1000000, "uint256"]
+        ],
+        decryptedOutput => expect(decryptedOutput).toEqual("")
+      );
+      await testComputeHelper(
+        enigma,
+        accounts[0],
+        scAddr,
+        "add_millionaire(bytes32,uint256)",
+        [
+          [millionaire2, "bytes32"],
+          [2000000, "uint256"]
+        ],
+        decryptedOutput => expect(decryptedOutput).toEqual("")
+      );
+    },
+    constants.TIMEOUT_COMPUTE
+  );
+
+  it(
+    "computeTask compute_richest",
+    async () => {
+      await testComputeHelper(enigma, accounts[0], scAddr, "compute_richest(uint256,uint256)", [], decryptedOutput =>
+        expect(decryptedOutput).toEqual(utils.remove0x(millionaire2))
+      );
+    },
+    constants.TIMEOUT_COMPUTE
   );
 });
