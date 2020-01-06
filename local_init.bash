5fbd4e6f28e8891617882072147be108c7031772
assafmo <assaf.morami@gmail.com>
2020-01-06 15:51:17 +0200

    refactor local_init.bash script

--- local_init.bash
+++ local_init.bash
@@ -5,11 +5,10 @@ set -xe
 CONTAINER_ID=$(docker container ls | grep enigmampc/client | cut -d' ' -f1)
 
 rm -rf ../build ~/.enigma /tmp/enigma
-mkdir -p ../build ~/.enigma /tmp/enigma
+mkdir -p ../build /tmp/enigma
 
-docker cp $CONTAINER_ID:/root/build/contracts ../build
-sed -e 's_http://contract_http://localhost_g;s_http://bootstrap_http://localhost_g' ../build/contracts/addresses.json > ../build/contracts/addresses.new
-mv ../build/contracts/addresses.new ../build/contracts/addresses.json
+docker cp "$CONTAINER_ID":/root/build/contracts ../build
+sed -i -e 's_http://contract_http://localhost_g;s_http://bootstrap_http://localhost_g' ../build/contracts/addresses.json
 
 if [ ! -f .env ]; then
     echo "SGX_MODE=SW" > .env
