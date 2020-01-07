abf63bd2e6a417e20f2d2399cc5066e481c52b5e
assafmo <assaf.morami@gmail.com>
2020-01-07 11:07:33 +0200

    calc each test has its own deploy

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -10,7 +10,6 @@ describe("calculator", () => {
   let accounts;
   let web3;
   let enigma;
-  let scAddr;
 
   beforeAll(async () => {
     web3 = new Web3(new Web3.providers.HttpProvider(ethNodeAddr));
@@ -25,18 +24,15 @@ describe("calculator", () => {
     expect(Enigma.version()).toEqual("0.0.1");
   });
 
-  it(
-    "deploy",
-    async () => {
-      const deployTask = await testDeployHelper(
-        enigma,
-        accounts[0],
-        path.resolve(__dirname, "../secretContracts/calculator.wasm")
-      );
-      scAddr = deployTask.scAddr;
-    },
-    constants.TIMEOUT_DEPLOY
-  );
+  let scAddr;
+  beforeEach(async () => {
+    const deployTask = await testDeployHelper(
+      enigma,
+      accounts[0],
+      path.resolve(__dirname, "../secretContracts/calculator.wasm")
+    );
+    scAddr = deployTask.scAddr;
+  }, constants.TIMEOUT_DEPLOY);
 
   it(
     "computeTask sub",
