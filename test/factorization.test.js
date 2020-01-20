0d8f37e115eadeb6121cb5a3ecb05400c1982acb
assafmo <assaf.morami@gmail.com>
2020-01-20 16:02:20 +0200

    make ready for 3rd 12-hour test

--- test/factorization.test.js
+++ test/factorization.test.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
 const Web3 = require("web3");
 const { Enigma } = require("../enigmaLoader");
 const { EnigmaContractAddress, EnigmaTokenContractAddress, proxyAddress, ethNodeAddr } = require("../contractLoader");
 const constants = require("../testConstants");
 
-const { testDeployHelper, testComputeHelper } = require("../scUtils");
+const { testDeployHelper, testComputeHelper, sleep } = require("../scUtils");
 
 describe("factorization", () => {
   let accounts;
@@ -14,6 +16,14 @@ describe("factorization", () => {
   beforeAll(async () => {
     web3 = new Web3(new Web3.providers.HttpProvider(ethNodeAddr));
     accounts = await web3.eth.getAccounts();
+
+    try {
+      const { data } = await axios.get(`${ethNodeAddr.replace(":9545", "")}:8001/faucet/ether?account=${accounts[0]}`);
+      console.log("Adding ETH to client successful:", data);
+    } catch (e) {
+      console.log("Error Adding ETH to client:", e);
+    }
+
     enigma = new Enigma(web3, EnigmaContractAddress, EnigmaTokenContractAddress, proxyAddress, {
       gas: 4712388,
       gasPrice: 100000000000,
@@ -22,55 +32,47 @@ describe("factorization", () => {
     enigma.admin();
     enigma.setTaskKeyPair("cupcake");
     expect(Enigma.version()).toEqual("0.0.1");
-  });
+  }, 30000);
 
   let scAddr;
-  beforeEach(async () => {
-    const deployTask = await testDeployHelper(
-      enigma,
-      accounts[0],
-      path.resolve(__dirname, "../secretContracts/factorization.wasm")
+  try {
+    scAddr = fs.readFileSync(".scAddr", "utf8");
+    console.log("Already deployed on", scAddr);
+  } catch (e) {
+    console.log("Not deployed. Deploying...");
+  }
+
+  if (!scAddr) {
+    it(
+      "deploy",
+      async () => {
+        const deployTask = await testDeployHelper(
+          enigma,
+          accounts[0],
+          path.resolve(__dirname, "../secretContracts/factorization.wasm")
+        );
+        scAddr = deployTask.scAddr;
+
+        fs.writeFileSync(".scAddr", scAddr, "utf8");
+
+        await sleep(30 * 1000);
+      },
+      constants.TIMEOUT_DEPLOY + 30 * 1000
     );
-    scAddr = deployTask.scAddr;
-  }, constants.TIMEOUT_DEPLOY);
+  }
 
   it(
-    "find_number_of_prime_factors",
+    "find_number_of_prime_factors(8972)",
     async () => {
-      const input = 8972;
-      const expectedOutput = Array.from(new Set(findPrimeFactors(input))).length;
-
       await testComputeHelper(
         enigma,
         accounts[0],
         scAddr,
         "find_number_of_prime_factors(uint64)",
-        [[input, "uint64"]],
-        decryptedOutput => expect(parseInt(decryptedOutput, 16)).toEqual(expectedOutput)
+        [[8972, "uint64"]],
+        decryptedOutput => expect(parseInt(decryptedOutput, 16)).toEqual(2)
       );
     },
     constants.TIMEOUT_COMPUTE
   );
 });
-
-// https://js-algorithms.tutorialhorizon.com/2015/09/27/find-all-the-prime-factors-for-the-given-number/
-function findPrimeFactors(num) {
-  const primeFactors = [];
-  while (num % 2 === 0) {
-    primeFactors.push(2);
-    num = num / 2;
-  }
-
-  const sqrtNum = Math.sqrt(num);
-  for (var i = 3; i <= sqrtNum; i++) {
-    while (num % i === 0) {
-      primeFactors.push(i);
-      num = num / i;
-    }
-  }
-
-  if (num > 2) {
-    primeFactors.push(num);
-  }
-  return primeFactors;
-}
